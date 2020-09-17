6b122c4d4cedc7c1b47862c91fd0e6231cbbbc85
ujw0l <bktujwol@gmail.com>
2020-09-17 14:01:56 +0545

    display user on modal window added

--- js/ctc_rating.js
+++ js/ctc_rating.js
@@ -1,7 +1,15 @@
 window.addEventListener('load', () => {
+    const overlay = new jsOverlay();
+    /**
+     * Script to handle user rating
+     */
 
-
-    ratePost = clickeEl => {
+    /**
+     * Ajax rate post
+     * 
+     * @param {*} clickeEl 
+     */
+    const ratePost = clickeEl => {
 
         let postId = clickeEl.getAttribute('data-type-id');
         let dataToSend = 'action=ctcUserRating&rating_data=' + JSON.stringify({ postId: postId, ratingType: clickeEl.getAttribute('data-type-rating') });
@@ -12,7 +20,7 @@ window.addEventListener('load', () => {
         xhttp.responseType = "text";
         xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;');
         xhttp.addEventListener('load', e => {
-            if (event.target.status >= 200 && event.target.status < 400) {
+            if (e.target.status >= 200 && e.target.status < 400) {
                 if ('notLoggedIn' == e.target.response) {
                     alert(ctcRating.notLoggedIn);
                 } else {
@@ -25,8 +33,13 @@ window.addEventListener('load', () => {
         xhttp.send(dataToSend);
     }
 
-    //function to update Rating based on server response
-    ctcUpdateRating = (serverResponse, productId, clickedEl) => {
+    /**
+     * update Rating based on server response
+     * @param serverResponse Response from server
+     * @param postId Post Id
+     * @param clickedEl Clicked element
+     */
+    const ctcUpdateRating = (serverResponse, postId, clickedEl) => {
         let scenario = clickedEl.getAttribute('data-type-scenario');
         let otherRating;
         switch (serverResponse) {
@@ -34,26 +47,26 @@ window.addEventListener('load', () => {
                 clickedEl.title = ctcRating.alreadyThumbedUp
                 clickedEl.classList.remove('ctcThumbDown')
                 clickedEl.classList.add('ctcUserThumbUp');
-                ctcCalcUpDown(productId, 'thumbsUp', scenario);
+                ctcCalcUpDown(postId, 'thumbsUp');
                 break;
             case 'thumbsDown':
                 clickedEl.title = ctcRating.alreadyThumbedDown
                 clickedEl.classList.remove('ctcThumbUp')
                 clickedEl.classList.add('ctcUserThumbDown');
                 clickedEl.style.fontSize = '25px'
-                ctcCalcUpDown(productId, 'thumbsDown', scenario);
+                ctcCalcUpDown(postId, 'thumbsDown');
                 break;
             case 'thumbsUpReversed':
                 clickedEl.title = ctcRating.alreadyThumbedDown
                 clickedEl.classList.add('ctcUserThumbDown');
                 clickedEl.style.fontSize = '25px';
 
-                otherRating = document.querySelector('#ctcThumbUp-' + productId);
+                otherRating = document.querySelector('#ctcThumbUp-' + postId);
                 otherRating.title = ctcRating.thumbUp
                 otherRating.classList.remove('ctcUserThumbUp')
                 otherRating.classList.add('ctcThumbUp')
                 otherRating.style.fontSize = '20px';
-                ctcCalcUpDown(productId, 'thumbsUpReversed', scenario);
+                ctcCalcUpDown(postId, 'thumbsUpReversed');
                 break;
             case 'thumbsDownReversed':
                 clickedEl.title = ctcRating.alreadyThumbedUp;
@@ -61,32 +74,37 @@ window.addEventListener('load', () => {
                 clickedEl.classList.add('ctcUserThumbUp');
                 clickedEl.style.fontSize = '25px';
 
-                otherRating = document.querySelector('#ctcThumbDown-' + productId);
+                otherRating = document.querySelector('#ctcThumbDown-' + postId);
                 otherRating.title = ctcRating.thumbDown;
                 otherRating.classList.remove('ctcUserThumbDown');
                 otherRating.classList.add('ctcThumbDown');
                 otherRating.style.fontSize = '20px';
-                ctcCalcUpDown(productId, 'thumbsDownReversed', scenario);
+                ctcCalcUpDown(postId, 'thumbsDownReversed', scenario);
                 break;
             case 'unThumbsUp':
                 clickedEl.title = ctcRating.thumbUp;
                 clickedEl.classList.remove('ctcUserThumbUp');
                 clickedEl.classList.add('ctcThumbUp');
                 clickedEl.style.fontSize = '20px'
-                ctcCalcUpDown(productId, 'unThumbsUp', scenario);
+                ctcCalcUpDown(postId, 'unThumbsUp');
                 break;
             case 'unThumbsDown':
                 clickedEl.title = ctcRating.thumbDown;
                 clickedEl.classList.remove('ctcUserThumbDown');
                 clickedEl.classList.add('ctcThumbDown');
                 clickedEl.style.fontSize = '20px';
-                ctcCalcUpDown(productId, 'unThumbsDown', scenario);
+                ctcCalcUpDown(postId, 'unThumbsDown');
                 break;
         }
     }
 
-    //function add substract thumup and thumbdown based on server respose
-    ctcCalcUpDown = (postId, action, scenario) => {
+    /**
+     * add substract thumup and thumbdown based on server respose
+     * 
+     * @param postId Post id
+     * @param action type of action to be carried out
+     */
+    const ctcCalcUpDown = (postId, action) => {
         let ratingUpElem, ratingDownElem, ctcNewThumbsUpCount, ctcNewThumbsDownCount;
         switch (action) {
             case 'thumbsUp':
@@ -137,8 +155,13 @@ window.addEventListener('load', () => {
     }
 
 
-    //function to javascript number format
-    addCommas = (nStr) => {
+    /**
+     * number format 
+     * 
+     * @param nStr number to be formatted
+     * @returns formatted number
+     */
+    const addCommas = (nStr) => {
         nStr += '';
         x = nStr.split('.');
         x1 = x[0];
@@ -151,8 +174,45 @@ window.addEventListener('load', () => {
     }
 
     let ratingArr = Array.from(document.querySelectorAll(['.ctcThumbUp', '.ctcThumbDown', '.ctcUserThumbUp', '.ctcUserThumbDown']));
-
     ratingArr.map(x => x.addEventListener('click', e => ratePost(e.target)))
+
+    /**
+     * 
+     * Script to display user who liked or disliked post
+     */
+
+
+    /**
+     * Get users based on stat
+     * @param elClicked Clicked stat display
+     */
+
+    const ctcDisplayUsers = elClicked => {
+
+        overlay.createOverlay({
+            ajaxUrl: ctcRating.ctcRatingAjaxUrl,
+            ajaxData: 'action=ctcGetUsers&post_id=' + elClicked.getAttribute('data-post-id') + '&user_to_get=' + elClicked.getAttribute('data-get'),
+            ajaxMethod: 'POST',
+            containerHt: 350,
+            containerWd: 200,
+        })
+    }
+
+    /**
+    * Display users who rated post
+    * 
+    */
+    let statArr = Array.from(document.querySelectorAll('.ctcThumbsUpStat,.ctcThumbsDownStat'));
+
+    statArr.map(x => x.addEventListener('click', e => {
+        let dataStatCount = 'thumb-up-count' == e.target.getAttribute('data-get') ? e.target.getAttribute('data-type-thumupcount') : e.target.getAttribute('data-type-thumdowncount');
+
+
+        if ('0' != dataStatCount) {
+            ctcDisplayUsers(e.target)
+        }
+
+    }));
 
 
 });
